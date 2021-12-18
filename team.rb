2ec98532465df9600309e07022a3e6f4eb0880c0
yuuki.oikawa2 <yuki.oikawa.g@gmail.com>
2021-12-18 13:42:04 +0000

    kadai-ruby-opp-1 commit

---
+++ team.rb
@@ -0,0 +1,37 @@
+class Team
+    #オブジェクトの変数
+    attr_accessor :name, :win, :lose, :draw
+    
+    #初期化
+    def initialize(name,win,lose,draw)
+        self.name=name
+        self.win=win
+        self.lose=lose
+        self.draw=draw
+    end
+    
+    #勝率計算
+    def calc_win_rate
+        self.win.to_f/(self.win.to_f+self.lose.to_f)   # return の省略
+    end
+    
+    #成績表示
+    def show_team_result
+        puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}、勝率は #{self.calc_win_rate}"
+    end
+    
+end
+
+Team_Giants = Team.new('Giants',67,45,8)    # 引数による初期化
+Team_Tigers = Team.new('Tigers',60,53,7)    # 引数による初期化
+Team_Dragons  = Team.new('Dragons',60,55,5)    # 引数による初期化
+Team_BayStars = Team.new('BayStars',56,58,6)    # 引数による初期化
+Team_Carp = Team.new('Carp',52,56,12)    # 引数による初期化
+Team_Swallows = Team.new('Swallows',41,69,10)    # 引数による初期化
+
+Team_Giants.show_team_result
+Team_Tigers.show_team_result 
+Team_Dragons.show_team_result
+Team_BayStars.show_team_result
+Team_Carp.show_team_result 
+Team_Swallows.show_team_result
