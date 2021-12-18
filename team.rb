1e32548a95a8fb84c70b68039fbac1fe7c1c3099
yuuki.oikawa2 <yuki.oikawa.g@gmail.com>
2021-12-18 15:08:20 +0000

    update kadai-ruby-opp-1 commit

--- team.rb
+++ team.rb
@@ -17,21 +17,21 @@ class Team
     
     #成績表示
     def show_team_result
-        puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}、勝率は #{self.calc_win_rate}"
+        puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は #{self.calc_win_rate}"
     end
     
 end
 
-Team_Giants = Team.new('Giants',67,45,8)    # 引数による初期化
-Team_Tigers = Team.new('Tigers',60,53,7)    # 引数による初期化
-Team_Dragons  = Team.new('Dragons',60,55,5)    # 引数による初期化
-Team_BayStars = Team.new('BayStars',56,58,6)    # 引数による初期化
-Team_Carp = Team.new('Carp',52,56,12)    # 引数による初期化
-Team_Swallows = Team.new('Swallows',41,69,10)    # 引数による初期化
+team_Giants = Team.new('Giants',67,45,8)    # 引数による初期化
+team_Tigers = Team.new('Tigers',60,53,7)    # 引数による初期化
+team_Dragons  = Team.new('Dragons',60,55,5)    # 引数による初期化
+team_BayStars = Team.new('BayStars',56,58,6)    # 引数による初期化
+team_Carp = Team.new('Carp',52,56,12)    # 引数による初期化
+team_Swallows = Team.new('Swallows',41,69,10)    # 引数による初期化
 
-Team_Giants.show_team_result
-Team_Tigers.show_team_result 
-Team_Dragons.show_team_result
-Team_BayStars.show_team_result
-Team_Carp.show_team_result 
-Team_Swallows.show_team_result
+team_Giants.show_team_result
+team_Tigers.show_team_result 
+team_Dragons.show_team_result
+team_BayStars.show_team_result
+team_Carp.show_team_result 
+team_Swallows.show_team_result
